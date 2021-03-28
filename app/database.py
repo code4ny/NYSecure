43f56e4926e476f9de3a148b4732d8c3d7279885
ljxr1337 <ljxr1337@gmail.com>
2021-03-28 15:25:03 +0800

    Created google login in test.py file, and created database for users

--- app/database.py
+++ app/database.py
@@ -1,5 +1,83 @@
 import os
+import requests
+import json
+from flask_login import UserMixin
 
 # replace with the path of the database.
-testing_database = ''
+testing_database = 'https://api.airtable.com/v0/appt1zYKCcPRO3xQW/'
 database_url = os.environ.get('DATABASE_URL', testing_database)
+# Retrieve airtable api key from https://airtable.com/account, login with code4ny google account
+airtablekey = os.environ.get('AIRTABLE_KEY', None)
+class DataStore:
+
+    def __init__(self):
+        self.url = database_url
+        self.headers = {"Authorization":  "Bearer " + airtablekey, "Content-Type" : "application/json"}
+    
+    def insert(self, table, record:dict):
+        """
+        Inserts record into table in database
+        """
+        upload_dict = {"records" : [{"fields": record}], "typecast": False}
+        upload_json = json.dumps(upload_dict)
+        response = requests.post(self.url + table, data=upload_json, headers=self.headers)
+
+    def get_records(self, table):
+        """
+        Returns all records from table in a list of dicts
+        """
+        params = ()
+        airtable_records = []
+        run = True
+        while run is True:
+            response = requests.get(self.url + table, params=params, headers=self.headers)
+            airtable_response = response.json()
+            airtable_records += (x["fields"] for x in airtable_response["records"])
+            if "offset" in airtable_response:
+                run = True
+                params = (("offset", airtable_response["offset"]),)
+            else:
+                run = False
+        return airtable_records
+
+    def get_records_by_param(self, table, params={}):
+        """
+        Returns all records from table in a list of dicts that fulfils params
+        """
+        response = requests.get(self.url + table, params=params, headers=self.headers)
+        airtable_response = response.json()
+        airtable_records = [x["fields"] for x in airtable_response["records"]]
+        return airtable_records
+
+
+    def delete(self, table, record):
+        pass
+
+class User(UserMixin):
+
+    def __init__(self, id_, name, email, type_, profile_pic):
+        self.id = id_
+        self.name = name
+        self.email = email
+        self.type = type_
+        self.profile_pic = profile_pic
+    
+    @classmethod
+    def get(cls, ds, user_id):
+        """
+        Returns a User object from a DataStore object, whose id is user_id.
+        Returns None if user does not exist in DataStore.
+        """
+        usr = ds.get_records_by_param("users", {"filterByFormula": f"id={user_id}"})
+        if len(usr) == 0:
+            return None
+        else:
+            usr = usr[0]
+            user = cls(usr["id"], usr["name"], usr["email"], usr["type"], usr["profile_pic"])
+            return user
+    
+    def to_db(self, ds):
+        """
+        Inserts a row with name, email, type, and profile_pic into "users" table in database in a DataStore object.
+        """
+        ds.insert("users", {"id": self.id, "email": self.email, "name": self.name, "type": self.type, "profile_pic": self.profile_pic})
---
+++ app/test.py
@@ -0,0 +1,97 @@
+# test file for google login. 
+
+from database import DataStore, User
+import json
+import os
+from flask import Flask, redirect, request, url_for
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+a = DataStore()
+# Retrieve client id and secret from https://console.cloud.google.com/apis/credentials?project=nysecure
+# Login with code4ny google account
+google_client_id = os.environ.get("GOOGLE_CLIENT_ID", None)
+google_client_secret = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+google_discovery_url = "https://accounts.google.com/.well-known/openid-configuration"
+def get_google_provider_cfg():
+    return requests.get(google_discovery_url).json()
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+login_manager = LoginManager()
+login_manager.init_app(app)
+client = WebApplicationClient(google_client_id)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(a, user_id)
+
+@app.route("/")
+def root():
+    if current_user.is_authenticated:
+        return (
+            f"""<p>Hello, {current_user.name}! You're logged in! Email: {current_user.email}</p>
+            <div><p>Google Profile Picture:</p>
+            <img src="{current_user.profile_pic}" alt="Google profile pic"></img></div>
+            <a class="button" href="/logout">Logout</a>"""
+        )
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
+
+@app.route("/login")
+def login():
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_url, headers, body = client.prepare_token_request(token_endpoint,\
+    authorization_response=request.url, redirect_url=request.base_url, code=code)
+    token_response = requests.post(token_url, headers=headers, data=body, auth=(google_client_id, google_client_secret))
+    client.parse_request_body_response(json.dumps(token_response.json()))
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    usrinfodict = userinfo_response.json()
+    if usrinfodict.get("email_verified"):
+        unique_id = usrinfodict["sub"]
+        users_email = usrinfodict["email"]
+        picture = usrinfodict["picture"]
+        users_name = f"{usrinfodict['given_name']} {usrinfodict['family_name']}"
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Assigns a type to the user and creates user object
+    if "Staff" in users_name:
+        type_ = "teacher"
+    else:
+        type_ = "student"
+    user = User(unique_id, users_name, users_email, type_, picture)
+    
+    # Adds user to database if it does not exist
+    if not User.get(a, unique_id):
+        user.to_db(a)
+
+    login_user(user)
+    return redirect(url_for("root"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("root"))
+
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc')
