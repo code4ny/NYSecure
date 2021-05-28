ad47d534c99e15d78027fb3cfd7482f669370f99
Poh Anson <pohanson979@gmail.com>
2021-05-28 14:12:48 +0800

    Finished get_location_data api

--- app/api.py
+++ app/api.py
@@ -1,8 +1,18 @@
 from app.config import BASE_API_PATH, DATABASE_URI
 from app import app
 from flask import jsonify
+from app.database import DataStore
+from app.location_variables import BLOCKS
+
+ds = DataStore()
 
 
 @app.route(BASE_API_PATH + "/locationdata")
 def get_location_data():
-    return jsonify()
+    datas = {}
+    for block in BLOCKS:
+        datas[block] = {}
+        for level in range(1, 6):
+            datas[block][level] = ds.get_summary(block=block, level="l" + str(level))
+
+    return jsonify(datas)
